d067498efa571cdc538cb2390cb83c88b62993c8
mangofsh <4iakhan@gmail.com>
2025-10-23 10:20:27 -0400

    Fix import statements in __init__.py

--- VimeoTools/__init__.py
+++ VimeoTools/__init__.py
@@ -1,4 +1,2 @@
 from . import deepgramTranscriber
 from . import videoDownloader
-from .videoDownloader import download_video, fetch_metadata_as_string
-from .deepgramTranscriber import transcribe_audio
