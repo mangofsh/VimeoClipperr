9e4d84843c57dc0d93803f334bf3961303cb96c4
mangofsh <4iakhan@gmail.com>
2025-10-23 10:10:25 -0400

    Updated import for deepgramTranscriber

--- VimeoTools/__init__.py
+++ VimeoTools/__init__.py
@@ -1,1 +1,4 @@
-
+from . import deepgramTranscriber
+from . import videoDownloader
+from .videoDownloader import download_video, fetch_metadata_as_string
+from .deepgramTranscriber import transcribe_audio
